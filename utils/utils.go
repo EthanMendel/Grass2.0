10372a2177ddc43d94878ed719910b68e38b9d00
Ethan Mendel <ethanmendel@Ethans-MacBook-Pro.local>
2019-11-04 11:23:21 -0500

    change format for FmtDuration

--- utils/utils.go
+++ utils/utils.go
@@ -96,9 +96,9 @@ func FmtDuration(d time.Duration) string {
 	d -= s * time.Second
 	ms := d / time.Millisecond
 	if s == 0 {
-		return fmt.Sprintf("%02d", ms)
+		return fmt.Sprintf("%2.2f", ms)
 	} else {
-		return fmt.Sprintf("%02d:%02d", s, ms)
+		return fmt.Sprintf("%2.2f:%2.2f", s, ms)
 	}
 }
 
